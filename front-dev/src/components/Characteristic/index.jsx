97c847631a006b7e878f15cac2d9437ad6c3f007
dontoouch me <tereh.tnvd@gmail.com>
2024-03-28 12:33:59 +0300

    start of development component Characteristic, build the path and implemented the inputs

--- front-dev/src/components/Characteristic/index.jsx
+++ front-dev/src/components/Characteristic/index.jsx
@@ -1,45 +1,126 @@
-import React, { useState } from "react";
+import "./style.css";
+
+import React, { useState , useEffect } from "react";
+import {
+  getRoomsThunk,
+  setRoomThunk,
+  deleteRoomsThunk,
+} from "../../redux/actions/mainThunks";
+import { connect } from "react-redux";
+import Select from "react-select";
 
 
 
-const Characteristic = ({active, setActive , data}) =>{
 
-// const [activeBtn,setActiveBtn] = useState(false)
-// let [inputValue,setInputValue]= useState('')
-// let [dataRow,setDataRow] = useState()
+const Characteristic = ({
+  rooms,
+  getRoomsThunk,
+
+}) => {
+  const [dataState, setDataState] = useState([]);
+  const [roomsData, setRoomsData] = useState([]);
+
+  useEffect(() => {
+    getRoomsThunk();
+    // fetch("http://192.168.11.57:18076/api/hostels/1/rooms", {
+    fetch("http://localhost:3001/room", {
+      headers: {
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("user"))["access_token"],
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRoomsData(data)
+        if (data !== undefined) {
+          data.forEach((item) => {
+            if (item.students.length > 0) {
+              item.students.forEach((st) => {
+                updateDataStudents({
+                  ...item,
+                  students: st,
+                });
+              });
+            }
+          });
+        }
+
+      })
+
+  }, []);
 
 
+  let tempArr = [];
 
+  const updateDataStudents = (room) => {
+    tempArr.push(room);
+    setDataState(tempArr)
+  };
 
-// const getSelectedRow = () => {
-  
-//   setActiveBtn(true)
-//   const selectedRow = data()
-//   console.log(selectedRow)
-//   if(selectedRow !== undefined) {
-//     setDataRow(selectedRow)
-//     setInputValue(selectedRow)
-//   }
-//   else {
-//     alert('Студент не выбран')
-//   }
-// }
+  const [currentName, setCurrentName] = useState("");
+  const [currentRoom, setCurrentRoom] = useState("");
 
+  const optionsName = 
+    dataState !==undefined
+      ? dataState.map((item) => {
+          return {
+            value: `${item.students.name} ${item.students.patronymic} ${item.students.surname}`,
+            label: `${item.students.name} ${item.students.patronymic} ${item.students.surname}`,
+          };
+        })
+      :"unknown";
 
-// const overWriteData = () => {
-//   setDataRow((currentData)=>({
-//     ...currentData,
-//     roomNumber: Number(inputValue.roomNumber) || Number(inputValue),
-//   }));
-//   console.log(dataRow)
-  
-// }
+  const optionsRoom =
+    roomsData !== undefined
+      ? roomsData.map((item) => {
+          return {
+            value: `${item.roomNumber} ${item.roomType === "LITTLE" ? "М" : "Б"}`,
+            label: `${item.roomNumber} ${item.roomType === "LITTLE" ? "М" : "Б"}`,
+          };
+        })
+      : "unknown";
 
     return (  
       <div>
-        <h3>Характеристика</h3>
+        <div className="header-char">
+          <h3>Характеристика</h3>
+          <div className="select-char">
+          <Select
+                className="select-content"
+                options={optionsName}
+                onChange={(newValue) => setCurrentName(newValue.value)}
+                value={
+                  currentName
+                    ? optionsName.find((c) => c.value === currentName)
+                    : ""
+                }
+              />
+          <Select
+                className="select-content"
+                options={optionsRoom}
+                onChange={(newValue) => setCurrentRoom(newValue.value)}
+                value={
+                  currentRoom
+                    ? optionsRoom.find((c) => c.value === currentRoom)
+                    : ""
+                }
+              />
+          </div>
+        </div>
       </div>
     )
 }
 
-export default Characteristic;
+let mapStateToProps = (state) => {
+  return {
+    rooms: state.mainPage.rooms,
+    // students: state.mainPage.students,
+  };
+};
+
+export default connect(mapStateToProps, {
+  getRoomsThunk,
+  setRoomThunk,
+  deleteRoomsThunk,
+  // getStudentsThunk,
+})(Characteristic);
